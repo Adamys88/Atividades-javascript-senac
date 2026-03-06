a7e736775c7b7f55b855e32608cb7cf56e44fcfe
ADAMYS LINCOLN BARROS CASTRO <adamys.lbcastro@senacsp.edu.br>
2026-03-06 10:33:44 -0300

    atividades 05 e 06

---
+++ 06-atividade/js/script.js
@@ -0,0 +1,17 @@
+function produzirBolos() {
+
+    let resultado = document.getElementById("resultado");
+
+    resultado.innerHTML = "";
+
+    for (let i = 1; i <= 10; i++) {
+        resultado.innerHTML += " Bolo de pote número " + i + " produzido <br>";
+    }
+} 
+  
+  
+  
+  
+  
+  
+  
