f461935fdf49efa3caff53ec9403a30dbdf6c5f6
ADAMYS LINCOLN BARROS CASTRO <adamys.lbcastro@senacsp.edu.br>
2026-03-06 11:34:08 -0300

    Atualização da atividade 06

--- 06-atividade/js/script.js
+++ 06-atividade/js/script.js
@@ -1,17 +1,39 @@
-function produzirBolos() {
+for (let i = 1; i <= 10; i++) {
 
-    let resultado = document.getElementById("resultado");
+    console.log("Bolo de pote número " + i + " produzido");
 
-    resultado.innerHTML = "";
+}
+const botaoReset = document.getElementById('btn-reset');
+const botao = document.getElementById('btnCalcular');
+const container = document.getElementById('lista-producao');
+const statusTexto = document.getElementById('status');
+
+// Botão de resetar o relatório
+botaoReset.addEventListener('click', function() {
+    
+    container.innerHTML = ''; 
+
+    statusTexto.innerText = "Aguardando produção...";
+    statusTexto.style.color = "#5d3a3a";
+    
+    console.log("Processo resetado!");
+});
+
+// Botão de Calcular
+botao.addEventListener('click', function() {
+    
+    container.innerHTML = "";
 
     for (let i = 1; i <= 10; i++) {
-        resultado.innerHTML += " Bolo de pote número " + i + " produzido <br>";
+        
+        const item = document.createElement('div');
+        item.className = 'item-bolo';
+        
+        item.innerHTML = `<strong>🍰 Bolo nº ${i}</strong><br>Status: Finalizado`;
+        
+        container.appendChild(item);
     }
-} 
-  
-  
-  
-  
-  
-  
-  
+
+    statusTexto.innerText = "Produção Finalizada! ✅";
+    statusTexto.style.color = "#2e7d32";
+});
