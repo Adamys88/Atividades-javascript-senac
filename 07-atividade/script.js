a0fd1f65ada357a7997a5969a86f186a7edd5849
SeuNome <adamys.lbcastro@senacsp.edu.br>
2026-03-12 08:45:47 -0300

    Atualização da atividade

---
+++ 07-atividade/script.js
@@ -0,0 +1,79 @@
+// 1 Sistemas de pedidos - Empresa de bolo de pote
+ 
+const sabores = ["Brigadeiro", "Cenoura", "Ninho", "Limão"];
+ 
+console.log(sabores);
+console.log(sabores.length);
+ 
+//Adicionando um novo sabor
+sabores.push("Baunilha");
+console.log(sabores);
+console.log(sabores.length);
+ 
+//Removendo Sabor
+sabores.pop();
+console.log("Baunilha está fora do estoque " + sabores);
+console.log(sabores.length);
+ 
+ 
+// 2 Controle de clientes - Confeitaria
+ 
+const nomes = ["Grazi", "Thiago", "Maria", "Marcos", "Isabelly"];
+ 
+console.log(nomes[0]);
+console.log(nomes[4]);
+ 
+//Conferindo clientes
+const cliente = nomes.includes("Carlos")
+console.log(cliente);
+ 
+console.log(nomes.lastIndexOf("Maria"));
+ 
+ 
+// 3 Lista de Encomendas - Bolo de pote
+const pedidosManha = [5, 15, 25];
+console.log(pedidosManha.length);
+ 
+const pedidosTarde = [35, 45, 55];
+const todosPedidos = pedidosManha.concat(pedidosTarde);
+console.log(todosPedidos);
+ 
+ 
+ 
+// 4 Controle de serviços - Ateliê de costura
+const servicos = ["barra de calça ", "ajuste de vestido", "reforma de camisa"];
+ 
+//adicionando serviço
+servicos.unshift("apertar calça folgada ");
+console.log(servicos);
+ 
+//removendo primeiro array
+const primeiroServico = servicos.shift();
+console.log(servicos);
+ 
+ 
+// 5 Agenda de pacientes - Psicólogo
+const consulta = [];
+ 
+consulta.push("Higor");
+consulta.push("Cauã");
+consulta.push("Vitor");
+consulta.push("Eliza");
+ 
+for(let i = 0; i < consulta.length; i++ ){
+    console.log("Ìndice " + i + ": " + consulta[i]);
+}
+ 
+ 
+// 6 Horário de atendimentos - Clínica de psicologia
+let matriz = [
+    [9, 13, 15],
+    [8, 12, 14]
+];
+ 
+for(let b = 0; b < matriz.length; b++){
+    for(let c = 0; c < matriz[b].length; c++){
+        console.log("[" + b + "][" + c + "] = " + matriz[b][c] )
+    }
+}
+ 
